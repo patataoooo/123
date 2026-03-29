67f510a4987b9fc9feadaf19ba5c176faed7a712
patataoooo <olegbleckwhite@gmail.com>
2026-03-29 20:20:45 +0300

    Add files via upload

--- settings.py
+++ settings.py
@@ -1,10 +1,14 @@
 class Settings:
     def __init__(self):
         self.bg_color = (25, 10, 105)
-        self.screen_width = 1200
+        self.screen_width = 1100
         self.screen_height = 600
         self.ship_speed =    1.5
         self.bullet_speed = 1.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 210, 60)
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
+
